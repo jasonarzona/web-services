d4d06ea41eb5a9f1b8ca87265608d49c76a3c762
jzun <103344507+jasonarzona@users.noreply.github.com>
2022-08-10 05:49:00 +0000

    switching over to vscode

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # web-services
 
 This is a repo to use as a starter for building a web services application with PHP on replit.  It includes James Bond film info as the sample data.  None of the web service code is included but will be added by working together in the IT121 class.
-
--- index.php
+++ index.php
@@ -1,32 +1,123 @@
 <html>
 <head>
-<title>Bond Web Service Demo</title>
+<title>Pokemon Web Service Demo</title>
 <style>
 	body {font-family:georgia;}
+
+  .film{
+border:1px solid #E77DC2;
+border-radius: 5px;
+padding: 5px;
+margin-bottom:5px;
+position:relative;
+}
+  
+  .pic{
+position:absolute;
+right:10px;
+top:10px;
+}
+
+  .pic img{
+max-width:50px;
+}
+  
 </style>
 <script src="https://code.jquery.com/jquery-latest.js"></script>
 
 <script type="text/javascript">
-$(document).ready(function() {  
 
-	$('.category').click(function(e){
-        e.preventDefault(); //stop default action of the link
-		cat = $(this).attr("href");  //get category from URL
-		loadAJAX(cat);  //load AJAX and parse JSON file
-	});
-});	
+function bondTemplate(film){
+  
+  return `
+    <div class = "film">
+        <b>Film</b>: ${film.Film}<br />
+        <b>Title</b>: ${film.Title}<br />
+        <b>Year</b>: ${film.Year}<br />
+        <b>Director</b>: ${film.Director}<br />
+        <b>Producers</b>: ${film.Producers}<br />
+        <b>Writers</b>: ${film.Writers}<br />
+        <b>Composer</b>: ${film.Composer}<br />
+        <b>Bond</b>: ${film.Bond}<br />  
+        <b>Budget</b>: ${film.Budget}<br />
+        <b>Box Office</b>: ${film.BoxOffice}<br />
+        <div class = "pic">
+          <img src = "thumbnails/${film.Image}" />
+        </div>
+  `;
+  
+}
+  
+$(document).ready(function() {
+  
+$('.category').click(function(e){
+e.preventDefault(); //stop default action of the link
+cat = $(this).attr("href");  //get category from URL
+  
+var request = $.ajax({
+url: "api.php?cat=" + cat,
+method: "GET",
+dataType: "json"
+});
+request.done(function( data ) {
+console.log(data);
+
+  /*
+  //using JSON.stringify we can view the data on the page
+  let myData = JSON.stringify(data,null,4);
+  myData = "<pre>" + myData + "</pre>";
+  $("#output").html(myData);
+  */
+
+  //use data.title to show the order of films
+  $("#filmtitle").html(data.title);
+
+  //clear the previous films
+  $("#films").html("");
+
+  //loop through data.films and add to #films div
+  $.each(data.films,function(i,item){
+    let myFilm = bondTemplate(item);
+    $("<div></div>").html(myFilm).appendTo("#films");
+  });
+  
+});
+request.fail(function(xhr, status, error ) {
+alert('Error - ' + xhr.status + ': ' + xhr.statusText);
+});
+});
+});
 
 
 </script>
 </head>
 	<body>
-	<h1>Bond Web Service</h1>
-		<a href="year" class="category">Bond Films By Year</a><br />
-		<a href="box" class="category">Bond Films By International Box Office Totals</a>
-		<h3 id="filmtitle">Title Will Go Here</h3>
+	<h1>Pokemon Web Service</h1>
+		<a href="year" class="category">Pokemon By Name</a><br/>
+		<a href="box" class="category">Pokemon By Type</a><br/>
+    <a href="year" class="category">Pokemon By Ability</a><br/>
+		<a href="box" class="category">Pokemon By Height</a><br/>
+    <a href="year" class="category">Pokemon By Weight</a><br/>
+		<h3 id="filmtitle">This project will display </h3>
 		<div id="films">
-			<p>Films will go here</p>
-		</div>
+      <!--
+      <div class = "film">
+            <b>Film</b>: 1<br />
+            <b>Title</b>: Dr. No<br />
+            <b>Year</b>: 1962<br />
+            <b>Director</b>: Terence Young<br />
+            <b>Producers</b>: Harry Saltzman and Albert R. Broccoli<br />
+            <b>Writers</b>: Richard Maibaum, Johanna Harwood and Berkely MatherRichard Maibaum, Johanna Harwood and Berkely Mather<br />
+            <b>Composer</b>: Monty Norman<br />
+            <b>Bond</b>: Sean Connery<br />  
+            <b>Budget</b>: $1,000,000.00<br />
+            <b>BoxOffice</b>: $59,567,035.00<br />
+            <div class = "pic">
+              <img src = "thumbnails/dr-no.jpg" />
+            </div>
+            -->
+      </div>
+    </div>
 		<div id="output">Results go here</div>
 	</body>
 </html>
